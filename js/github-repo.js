69768e3ea2f4a9a84e56635def4fd248b3163061
LCore <kiritor@hotmail.com>
2017-01-18 09:35:09 +0800

    调整最后push时间，格式化显示时间

--- js/github-repo.js
+++ js/github-repo.js
@@ -3,7 +3,23 @@
  * @Description: github-repo挂件插件
  */
 ;
+Date.prototype.Format = function (fmt) { //author: meizz
+    var o = {
+        "M+": this.getMonth() + 1, //月份
+        "d+": this.getDate(), //日
+        "h+": this.getHours(), //小时
+        "m+": this.getMinutes(), //分
+        "s+": this.getSeconds(), //秒
+        "q+": Math.floor((this.getMonth() + 3) / 3), //季度
+        "S": this.getMilliseconds() //毫秒
+    };
+    if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+    for (var k in o)
+    if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+    return fmt;
+};
 (function($, window, document, undefined) {
+
     //插件初始化
     function init(target, options) {
         var settings = $.extend({}, $.fn.githubRepo.defaults, options);
@@ -16,23 +32,24 @@
             url: url,
             async: false,
             success: function(data, textStatus, jqXHR) {
-                console.info(data);
                 var repoTitle = "<div class='card-image-cell'><h3>" + data.name + "</h3></div>"
                 var $card = "<div class='card-image'>" + repoTitle + "</div>";
-                $(target).append($card);
+                //$(target).append($card);
                 //caption
                 var des = data.description;
-                if(des.length>20){
-                    des = des.substring(0,20)+"......";
+                if (des.length > 20) {
+                    des = des.substring(0, 20) + "......";
                 }
                 var $cardDescription = "<div class='card-description'><p>" + des + "</p></div>";
                 var $cardText = "<div class='card-text'>" + "<span class='meta-info' title='" + data.stargazers_count + " stars'>" + "<i class='fa fa-star' aria-hidden='true'></i>&nbsp;" + data.stargazers_count + "</span>";
                 $cardText += "<span class='meta-info' title='" + data.forks_count + " stars'>" + "<i class='fa fa-code-fork' aria-hidden='true'></i>&nbsp;" + data.forks_count + "</span>";
-                $cardText += "<span class='meta-info' title='" + data.updated_at + " stars'>" + "<i class='fa fa-clock-o' aria-hidden='true'></i>&nbsp;" + data.updated_at + "</span>";
+                $cardText += "<span class='meta-info' title='" + data.subscribers_count + " counts'>" + "<i class='fa fa-eye' aria-hidden='true'></i>&nbsp;" + data.subscribers_count + "</span>";
+                $cardText += "<span class='meta-info' title='" + new Date(data.pushed_at).Format("yyyy-MM-dd") + "'>" + "<i class='fa fa-clock-o' aria-hidden='true'></i>&nbsp;" + new Date(data.pushed_at).Format("yyyy-MM-dd") + "</span>";
 
                 $cardText += "</div>";
                 var $caption = "<div class='caption'>" + $cardDescription + $cardText + "</div>";
-                $(target).append($caption);
+                var href = "<a href='" + data.html_url + "' target='_blank'>" + $card + $caption;
+                $(target).append(href);
             }
         });
     }
